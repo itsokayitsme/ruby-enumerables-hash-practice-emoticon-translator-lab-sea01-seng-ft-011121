36cc4df277a129d3996ea4cb3fb4f0ac10a471de
Johnny Cao <jcao0928@gmail.com>
2021-01-09 04:00:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,15 +20,11 @@ def get_english_meaning(path, emoticon)
     end
   end
   return "Sorry, that emoticon was not found"
-  end
 end
 
 
-def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
-library = load_library(file_path)
-  if library["get_meaning"].include?(japanese_emoticon) 
-    library["get_meaning"][japanese_emoticon]
-  else
+def get_japanese_emoticon(path, emoticon)
+  load_library(path).each {|key, va;ie| if value [:english] == emoticon then return value
     "Sorry, that emoticon was not found"
   end
 end 	
